73ec8f26017fd0e473d915b50b02efb97e7ff583
Harsh Singh <hi.harsh@protonmail.ch>
2021-09-19 19:40:52 -0400

    Added type decs to lib + fixed package files

---
+++ index.mjs
@@ -0,0 +1,248 @@
+const Aquamarine = {
+    aquamarine1: 'hsl(170, 100%, 15%)',
+    aquamarine2: 'hsl(170, 86%, 22%)',
+    aquamarine3: 'hsl(170, 78%, 27%)',
+    aquamarine4: 'hsl(170, 65%, 36%)',
+    aquamarine5: 'hsl(170, 58%, 42%)',
+    aquamarine6: 'hsl(170, 54%, 48%)',
+    aquamarine7: 'hsl(170, 59%, 54%)',
+    aquamarine8: 'hsl(170, 59%, 63%)',
+    aquamarine9: 'hsl(170, 65%, 69%)',
+    aquamarine10: 'hsl(170, 49%, 81%)',
+};
+
+const Citrus = {
+    citrus1: 'hsl(36, 100%, 32%)',
+    citrus2: 'hsl(36, 100%, 38%)',
+    citrus3: 'hsl(36, 100%, 42%)',
+    citrus4: 'hsl(36, 100%, 46%)',
+    citrus5: 'hsl(36, 100%, 50%)',
+    citrus6: 'hsl(36, 100%, 55%)',
+    citrus7: 'hsl(36, 89%, 56%)',
+    citrus8: 'hsl(36, 100%, 62%)',
+    citrus9: 'hsl(36, 100%, 73%)',
+    citrus10: 'hsl(36, 100%, 80%)',
+};
+
+const Cyan = {
+    cyan1: 'hsl(198, 80%, 35%)',
+    cyan2: 'hsl(198, 73%, 41%)',
+    cyan3: 'hsl(198, 65%, 47%)',
+    cyan4: 'hsl(198, 67%, 54%)',
+    cyan5: 'hsl(198, 80%, 59%)',
+    cyan6: 'hsl(198, 88%, 63%)',
+    cyan7: 'hsl(198, 100%, 69%)',
+    cyan8: 'hsl(198, 100%, 74%)',
+    cyan9: 'hsl(198, 100%, 80%)',
+    cyan10: 'hsl(198, 100%, 86%)',
+};
+
+const ForestGreen = {
+    forestGreen1: 'hsl(184, 96%, 21%)',
+    forestGreen2: 'hsl(184, 88%, 25%)',
+    forestGreen3: 'hsl(184, 77%, 31%)',
+    forestGreen4: 'hsl(184, 71%, 37%)',
+    forestGreen5: 'hsl(184, 68%, 41%)',
+    forestGreen6: 'hsl(184, 70%, 44%)',
+    forestGreen7: 'hsl(184, 64%, 49%)',
+    forestGreen8: 'hsl(184, 69%, 52%)',
+    forestGreen9: 'hsl(184, 64%, 70%)',
+    forestGreen10: 'hsl(184, 56%, 79%)',
+};
+
+const Green = {
+    green1: 'hsl(138, 54%, 24%)',
+    green2: 'hsl(138, 57%, 31%)',
+    green3: 'hsl(138, 53%, 36%)',
+    green4: 'hsl(138, 48%, 41%)',
+    green5: 'hsl(138, 46%, 46%)',
+    green6: 'hsl(138, 46%, 52%)',
+    green7: 'hsl(138, 55%, 56%)',
+    green8: 'hsl(138, 62%, 60%)',
+    green9: 'hsl(138, 69%, 66%)',
+    green10: 'hsl(138, 70%, 79%)',
+};
+
+const HotPink = {
+    hotPink1: 'hsl(330, 69%, 32%)',
+    hotPink2: 'hsl(330, 64%, 36%)',
+    hotPink3: 'hsl(330, 58%, 42%)',
+    hotPink4: 'hsl(330, 54%, 47%)',
+    hotPink5: 'hsl(330, 58%, 53%)',
+    hotPink6: 'hsl(330, 65%, 57%)',
+    hotPink7: 'hsl(330, 78%, 64%)',
+    hotPink8: 'hsl(330, 96%, 72%)',
+    hotPink9: 'hsl(330, 92%, 85%)',
+    hotPink10: 'hsl(330, 100%, 86%)',
+};
+
+const Lime = {
+    lime1: 'hsl(97, 100%, 30%)',
+    lime2: 'hsl(98, 100%, 33%)',
+    lime3: 'hsl(97, 100%, 36%)',
+    lime4: 'hsl(97, 88%, 40%)',
+    lime5: 'hsl(97, 71%, 48%)',
+    lime6: 'hsl(97, 75%, 54%)',
+    lime7: 'hsl(97, 80%, 58%)',
+    lime8: 'hsl(97, 96%, 61%)',
+    lime9: 'hsl(97, 99%, 72%)',
+    lime10: 'hsl(98, 98%, 83%)',
+};
+
+const NavyBlue = {
+    navyBlue1: 'hsl(238, 43%, 33%)',
+    navyBlue2: 'hsl(238, 42%, 39%)',
+    navyBlue3: 'hsl(239, 37%, 46%)',
+    navyBlue4: 'hsl(238, 39%, 53%)',
+    navyBlue5: 'hsl(238, 47%, 58%)',
+    navyBlue6: 'hsl(238, 61%, 67%)',
+    navyBlue7: 'hsl(238, 81%, 76%)',
+    navyBlue8: 'hsl(238, 98%, 82%)',
+    navyBlue9: 'hsl(238, 100%, 86%)',
+    navyBlue10: 'hsl(238, 100%, 91%)',
+};
+
+const Orange = {
+    orange1: 'hsl(23, 86%, 38%)',
+    orange2: 'hsl(23, 76%, 42%)',
+    orange3: 'hsl(23, 70%, 45%)',
+    orange4: 'hsl(23, 66%, 50%)',
+    orange5: 'hsl(23, 76%, 55%)',
+    orange6: 'hsl(23, 82%, 59%)',
+    orange7: 'hsl(23, 89%, 64%)',
+    orange8: 'hsl(23, 100%, 71%)',
+    orange9: 'hsl(23, 100%, 79%)',
+    orange10: 'hsl(23, 100%, 84%)',
+};
+
+const Pink = {
+    pink1: 'hsl(306, 70%, 42%)',
+    pink2: 'hsl(306, 63%, 47%)',
+    pink3: 'hsl(306, 60%, 51%)',
+    pink4: 'hsl(306, 66%, 59%)',
+    pink5: 'hsl(306, 70%, 64%)',
+    pink6: 'hsl(306, 72%, 69%)',
+    pink7: 'hsl(306, 77%, 73%)',
+    pink8: 'hsl(306, 83%, 80%)',
+    pink9: 'hsl(306, 100%, 86%)',
+    pink10: 'hsl(306, 100%, 91%)',
+};
+
+const Purple = {
+    purple1: 'hsl(260, 50%, 35%)',
+    purple2: 'hsl(260, 46%, 42%)',
+    purple3: 'hsl(260, 43%, 48%)',
+    purple4: 'hsl(260, 43%, 53%)',
+    purple5: 'hsl(260, 50%, 59%)',
+    purple6: 'hsl(260, 53%, 63%)',
+    purple7: 'hsl(260, 66%, 70%)',
+    purple8: 'hsl(260, 77%, 75%)',
+    purple9: 'hsl(260, 100%, 83%)',
+    purple10: 'hsl(260, 100%, 89%)',
+};
+
+const Red = {
+    red1: 'hsl(358, 70%, 42%)',
+    red2: 'hsl(360, 66%, 46%)',
+    red3: 'hsl(0, 66%, 50%)',
+    red4: 'hsl(3, 78%, 55%)',
+    red5: 'hsl(4, 86%, 58%)',
+    red6: 'hsl(4, 86%, 62%)',
+    red7: 'hsl(4, 94%, 66%)',
+    red8: 'hsl(2, 95%, 74%)',
+    red9: 'hsl(359, 92%, 85%)',
+    red10: 'hsl(351, 82%, 89%)',
+};
+
+const Rose = {
+    rose1: 'hsl(4, 78%, 38%)',
+    rose2: 'hsl(4, 74%, 40%)',
+    rose3: 'hsl(4, 72%, 44%)',
+    rose4: 'hsl(4, 67%, 48%)',
+    rose5: 'hsl(4, 70%, 53%)',
+    rose6: 'hsl(4, 80%, 57%)',
+    rose7: 'hsl(4, 89%, 62%)',
+    rose8: 'hsl(4, 92%, 69%)',
+    rose9: 'hsl(4, 100%, 76%)',
+    rose10: 'hsl(3, 100%, 85%)',
+};
+
+const Rusty = {
+    rusty1: 'hsl(43, 100%, 33%)',
+    rusty2: 'hsl(43, 100%, 34%)',
+    rusty3: 'hsl(43, 100%, 38%)',
+    rusty4: 'hsl(43, 100%, 44%)',
+    rusty5: 'hsl(43, 100%, 47%)',
+    rusty6: 'hsl(43, 100%, 54%)',
+    rusty7: 'hsl(43, 100%, 59%)',
+    rusty8: 'hsl(43, 100%, 66%)',
+    rusty9: 'hsl(43, 100%, 77%)',
+    rusty10: 'hsl(43, 100%, 82%)',
+};
+
+const SkyBlue = {
+    skyBlue1: 'hsl(217, 80%, 35%)',
+    skyBlue2: 'hsl(217, 76%, 39%)',
+    skyBlue3: 'hsl(217, 66%, 47%)',
+    skyBlue4: 'hsl(217, 63%, 51%)',
+    skyBlue5: 'hsl(217, 72%, 58%)',
+    skyBlue6: 'hsl(217, 84%, 64%)',
+    skyBlue7: 'hsl(216, 100%, 70%)',
+    skyBlue8: 'hsl(216, 100%, 76%)',
+    skyBlue9: 'hsl(217, 100%, 81%)',
+    skyBlue10: 'hsl(217, 100%, 87%)',
+};
+
+const Yellow = {
+    yellow1: 'hsl(52, 100%, 32%)',
+    yellow2: 'hsl(52, 100%, 36%)',
+    yellow3: 'hsl(52, 100%, 39%)',
+    yellow4: 'hsl(52, 100%, 41%)',
+    yellow5: 'hsl(52, 100%, 43%)',
+    yellow6: 'hsl(52, 100%, 46%)',
+    yellow7: 'hsl(54, 100%, 50%)',
+    yellow8: 'hsl(52, 100%, 60%)',
+    yellow9: 'hsl(54, 100%, 71%)',
+    yellow10: 'hsl(54, 100%, 81%)',
+};
+
+const Black = {
+    black1: 'hsl(0, 0%, 0%)',
+    black2: 'hsl(0, 0%, 10%)',
+    black3: 'hsl(0, 0%, 12%)',
+    black4: 'hsl(0, 0%, 17%)',
+    black5: 'hsl(0, 0%, 20%)',
+    black6: 'hsl(0, 0%, 24%)',
+    black7: 'hsl(0, 0%, 26%)',
+    black8: 'hsl(0, 0%, 30%)',
+    black9: 'hsl(0, 0%, 31%)',
+    black10: 'hsl(0, 0%, 35%)',
+};
+
+const Grey = {
+    grey1: 'hsl(0, 0%, 36%)',
+    grey2: 'hsl(0, 0%, 41%)',
+    grey3: 'hsl(0, 0%, 47%)',
+    grey4: 'hsl(0, 0%, 51%)',
+    grey5: 'hsl(0, 0%, 55%)',
+    grey6: 'hsl(0, 0%, 58%)',
+    grey7: 'hsl(0, 0%, 65%)',
+    grey8: 'hsl(0, 0%, 68%)',
+    grey9: 'hsl(0, 0%, 70%)',
+    grey10: 'hsl(0, 0%, 73%)',
+};
+
+const White = {
+    white1: 'hsl(0, 0%, 58%)',
+    white2: 'hsl(0, 0%, 61%)',
+    white3: 'hsl(0, 0%, 67%)',
+    white4: 'hsl(0, 0%, 71%)',
+    white5: 'hsl(0, 0%, 76%)',
+    white6: 'hsl(0, 0%, 81%)',
+    white7: 'hsl(0, 0%, 85%)',
+    white8: 'hsl(0, 0%, 90%)',
+    white9: 'hsl(0, 0%, 98%)',
+    white10: 'hsl(0, 0%, 100%)',
+};
+
+export { Aquamarine, Black, Citrus, Cyan, ForestGreen, Green, Grey, HotPink, Lime, NavyBlue, Orange, Pink, Purple, Red, Rose, Rusty, SkyBlue, White, Yellow };
